f88e7ff3bc56120a28c30a378a525200744750b0
Felipe <felipetermometria@hotmail.com>
2023-12-13 18:54:54 -0300

    Api test - Rio de Janeiro

--- app/src/main/java/com/example/app_weather/ServiceAPI.kt
+++ app/src/main/java/com/example/app_weather/ServiceAPI.kt
@@ -3,9 +3,13 @@ package com.example.app_weather
 import retrofit2.http.GET
 
 interface ServiceAPI {
-    //https://api.hgbrasil.com/weather?key=ac130002
+    //https://api.hgbrasil.com/weather?woeid=455825
 
-    @GET("/weather?key=ac130002")
+    @GET("/weather?woeid=455825")
     suspend fun getServiceAPI() : WeatherData
 
+
+
+
 }
+
